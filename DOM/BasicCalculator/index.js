d7ed37a0caa7c07ec0dbfc02cfcb99c4f7b09614
rstoyanovv <115493787+rstoyanovv@users.noreply.github.com>
2023-07-27 11:32:21 +0300

    Add files via upload

---
+++ DOM/BasicCalculator/index.js
@@ -0,0 +1,40 @@
+function performOperation(operation) {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert('Please enter valid numbers.');
+        return;
+    }
+
+    let result;
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiplication':
+            result = num1 * num2;
+            break;
+        case 'division':
+            result = num1 / num2;
+            break;
+        default:
+            alert('Invalid operation.');
+            return;
+    }
+
+    document.getElementById('result').textContent = result;
+
+    const imageURL = 'meme.jpg';
+    document.getElementById('resultImage').src = imageURL;
+
+    // Show the image element
+    document.getElementById('resultImage').style.display = 'block';
+
+    // Resize the image to 200x200 (you can adjust the size as needed)
+    document.getElementById('resultImage').style.width = '355px';
+    document.getElementById('resultImage').style.height = '237px';
+}
