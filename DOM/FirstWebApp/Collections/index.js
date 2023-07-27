a4f53dbb882595f4cabaaf85af13882794e8a125
rstoyanovv <115493787+rstoyanovv@users.noreply.github.com>
2023-07-27 10:40:14 +0300

    Delete DOM/FirstWebApp/Collections directory

--- DOM/FirstWebApp/Collections/index.js
+++
@@ -1,6 +0,0 @@
-function extractText() {
-    let ulElement = document.getElementById("list");
-
-    let textAreaElement = document.getElementById("result");
-    textAreaElement.textContent = ulElement.textContent;
-};
