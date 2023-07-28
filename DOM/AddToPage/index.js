347996d5d72d12037d9879839f37c2fbbcebe88f
rstoyanovv <115493787+rstoyanovv@users.noreply.github.com>
2023-07-28 10:48:14 +0300

    Update index.js

--- DOM/AddToPage/index.js
+++ DOM/AddToPage/index.js
@@ -6,3 +6,13 @@ function addCar() {
     liElement.textContent = inputElement.value; 
     ulElement.appendChild(liElement);
 }
+
+function deleteCar() {
+    let carInputElement = document.getElementById('car');
+    let carsInList = document.querySelectorAll('li');
+
+    let carArray = Array.from(carsInList);
+
+    let searchCar = carArray.find(x => x.textContent == carInputElement.value);
+    searchCar.remove();
+}
